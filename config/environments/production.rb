650f3e9425d6223418502271cf41bd50b15fa3c6
tayeb83 <tayeb.merabti@gmail.com>
2012-06-03 16:16:45 +0200

    js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,17 +19,7 @@ Arabproverbe::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "www.example.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: 'tayeb.merabti',
-  password: 'bosser83'
-}  
+  
 
 
 # Defaults to Rails.root.join("public/assets")
